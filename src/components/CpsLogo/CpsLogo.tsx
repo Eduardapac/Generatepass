f9472020ce92b8321007166d8b7343877a818aa5
Eduardapac <eduardaleitepac@gmail.com>
2024-08-24 11:23:25 -0300

    deu bom 24/08

--- src/components/CpsLogo/CpsLogo.tsx
+++ src/components/CpsLogo/CpsLogo.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import {Text, View } from 'react-native';
+import {Text, View, Image } from 'react-native';
 import {styles} from './CpsLogoStyles';
+import logo from '../../../assets/logocps.png'
 
 export function CpsLogo(){
     return(
-        <View style={styles.container}>
-            <Text>CPS PASS GENERATION</Text>
+        <View>
+            <Text style={styles.title}>CPS PASS GENERATION</Text>
+            <Image 
+            source={logo}
+            style={{
+                resizeMode:'contain',
+                height: 180,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            }}
+            />
         </View>
     );
 }
